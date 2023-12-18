b8566e279072346b676d4b3846b087afa9cc82a2
shridhamdeveloper <shridham55@gmail.com>
2023-12-18 11:54:03 +0530

    #86

--- controllers/class-gnpub-feed.php
+++ controllers/class-gnpub-feed.php
@@ -240,9 +240,11 @@ class GNPUB_Feed {
 					continue;
 				}
 
-				$pos = strpos( $content, $image[0] );
-				if ( $pos !== false ) {
-					$content = substr_replace( $content, '', $pos, strlen( $image[0] ) );
+				if(isset($image[0]) && is_string($image[0])){
+					$pos = strpos( $content, $image[0] );
+					if ( $pos !== false ) {
+						$content = substr_replace( $content, '', $pos, strlen( $image[0] ) );
+					}
 				}
 			}
 		}
